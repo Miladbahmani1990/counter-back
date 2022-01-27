2750e4c429a04d16ce9e0e358ea97b0564d8f4ce
milad <soushiyanet.1990@gmail.com>
2022-01-27 12:30:02 +0330

    counter-back

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
+// import react from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var interval;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//////////// state ///////////////////
+ //  برای نمایش تغییرات مقادیر در صفحه 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Timer1 extends React.Component{ 
+  constructor(){
+    super();
+    this.state={
+      time: 10
+    }
+  }
+
+  componentDidMount(){           //این تابع برای اولین بار اجرا میشود 
+
+    interval =setInterval(() => {
+      this.setState({
+        time: this.state.time - 1
+      })
+    }, 1000); 
+  }
+
+
+  componentDidUpdate(){                 //اجرا میشود و داخلش شرط گذاشتیم render   این تابع بعد از 
+    if (this.state.time == 0){
+      clearInterval(interval)
+    }
+  }
+
+  render(){
+   
+
+    return(
+      <h3>
+          it is {this.state.time}
+      </h3>
+    )
+  }
+}
+
+class Word1 extends React.Component{
+  render(){
+    return(
+      <h1>
+          timer by state
+      </h1>
+    )
+  }
+}
+
+class App1 extends React.Component{
+
+  render(){
+    return (
+
+      <div>
+
+        
+        <Word1/>
+        <Timer1/>
+        
+  
+      </div>
+    )
+  }
+}
+
+// const mytick=()=>{
+
+ReactDOM.render(<App1/>,document.getElementById("root"));
